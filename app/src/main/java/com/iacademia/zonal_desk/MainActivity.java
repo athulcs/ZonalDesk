6fb6af6448d5a64943ace2907a37fbbdc799c8c1
Athul CS <athulcs.cs@gmail.com>
2018-06-22 01:59:08 +0530

    Initial commit

---
+++ app/src/main/java/com/iacademia/zonal_desk/MainActivity.java
@@ -0,0 +1,13 @@
+package com.iacademia.zonal_desk;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
