47720b52fbf034ae7889dcbaebdd1ed5076c2948
Athul CS <athulcs.cs@gmail.com>
2018-07-04 22:20:10 +0530

    Apk generated

--- app/src/main/java/com/iacademia/zonal_desk/RegisterActivity.java
+++ app/src/main/java/com/iacademia/zonal_desk/RegisterActivity.java
@@ -2,6 +2,8 @@ package com.iacademia.zonal_desk;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -9,5 +11,8 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+    }
+    void dbInsert(View view){
+        Toast.makeText(this,"Pushed to database successfully",Toast.LENGTH_SHORT).show();
     }
 }
